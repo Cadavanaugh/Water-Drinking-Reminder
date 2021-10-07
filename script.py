04d504830d5bc2d28882bc3bb95505b466a58c06
Jovane Castro <jovanecastro12@gmail.com>
2021-10-07 19:19:41 -0300

    Added time
    The program now shows the time when I drank water.

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 from pygame import mixer
 from random import randint
 from time import sleep
+from os import system, name
+from datetime import datetime
 
 def som(path):
     mixer.init()
@@ -9,7 +11,11 @@ def som(path):
 
 interval = randint(1,1800) #A random interval from 1s to 30min
 
+system('cls' if name == 'nt' else 'clear')
+
 while True:
     sleep(interval)
+    time = datetime.now().strftime("%H:%M:%S")
+    print(f"Water drank at {time}")
     som('Water sound effect.mp3')
     sleep(6) #Sound effect takes 6s to finish
