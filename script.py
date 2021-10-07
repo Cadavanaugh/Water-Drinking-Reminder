a78aaaea45412fb6df164cc73a5f635f3f2077f4
Jovane Castro <jovanecastro12@gmail.com>
2021-10-07 19:05:22 -0300

    Comments

--- script.py
+++ script.py
@@ -7,9 +7,9 @@ def som(path):
     mixer.music.load(path)
     mixer.music.play()
 
-interval = randint(1,1800)
+interval = randint(1,1800) #A random interval from 1s to 30min
 
 while True:
     sleep(interval)
     som('Water sound effect.mp3')
-    sleep(6) #Sound effect takes 6s
+    sleep(6) #Sound effect takes 6s to finish
