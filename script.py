a9c84a474f41acd92ec90998ba79171d10cc614b
Jovane Castro <60260322+Cadavanaugh@users.noreply.github.com>
2021-10-27 01:40:28 -0300

    Don't actually need to re-seed at every loop.

--- script.py
+++ script.py
@@ -1,9 +1,8 @@
 from pygame import mixer
-from random import randint, seed
+from random import randint
 from time import sleep
 from os import system, name
 from datetime import datetime
-from time import time
 
 def som(path):
     mixer.init()
@@ -15,12 +14,11 @@ print("="*10,"WATER DRINKING REMINDER","="*10)
 
 x = 0 
 while True:
-    seed(time()) # Create true randomness
     interval = randint(1,1800) # A random interval from 1s to 30min
     sleep(interval)
-    clock = datetime.now().strftime("%H:%M:%S")
+    clock_time = datetime.now().strftime("%H:%M:%S")
     
     x+=1
-    print(f"Water drank at {clock} [{x}]")
+    print(f"Water drank at {clock_time} [{x}]")
     som('Water sound effect.mp3')
     sleep(6) # Sound effect takes 6s to finish
