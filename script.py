8f81d9cadde2bc2f1019e618265ab29b857729de
Jovane Castro <60260322+Cadavanaugh@users.noreply.github.com>
2021-10-27 01:10:49 -0300

    Making it truely random.

--- script.py
+++ script.py
@@ -1,23 +1,24 @@
 from pygame import mixer
-from random import randint
+from random import randint, seed
 from time import sleep
 from os import system, name
 from datetime import datetime
+from time import time
 
 def som(path):
     mixer.init()
     mixer.music.load(path)
     mixer.music.play()
 
-interval = randint(1,1800) #A random interval from 1s to 30min
-
 system('cls' if name == 'nt' else 'clear')
 
 x = 0 
 while True:
+    seed(time()) # Create true randomness
+    interval = randint(1,1800) # A random interval from 1s to 30min
     x+=1
     sleep(interval)
     time = datetime.now().strftime("%H:%M:%S")
     print(f"Water drank at {time} [{x}]")
     som('Water sound effect.mp3')
-    sleep(6) #Sound effect takes 6s to finish
+    sleep(6) # Sound effect takes 6s to finish
