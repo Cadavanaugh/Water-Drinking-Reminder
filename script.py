433b782f64eaef748e0298b72399879a398d970e
Jovane Castro <60260322+Cadavanaugh@users.noreply.github.com>
2021-10-27 01:29:43 -0300

    Fixed 'time' variable

--- script.py
+++ script.py
@@ -17,9 +17,10 @@ x = 0
 while True:
     seed(time()) # Create true randomness
     interval = randint(1,1800) # A random interval from 1s to 30min
-    x+=1
     sleep(interval)
-    time = datetime.now().strftime("%H:%M:%S")
-    print(f"Water drank at {time} [{x}]")
+    clock = datetime.now().strftime("%H:%M:%S")
+    
+    x+=1
+    print(f"Water drank at {clock} [{x}]")
     som('Water sound effect.mp3')
     sleep(6) # Sound effect takes 6s to finish
